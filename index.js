63e6a6c7c2f2e9bb5bb2b1d8b760dea6ba657599
mekkkwiz <themakza0021@gmail.com>
2023-03-13 15:08:25 +0700

    comment unused code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const config = require("./server/config/keys");
+// const config = require("./server/config/keys");
 // const mongoose = require("mongoose");
 // mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 //   .then(() => console.log('MongoDB Connected...'))
